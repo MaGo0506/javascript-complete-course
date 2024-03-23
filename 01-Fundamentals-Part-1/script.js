3495e72c7a83e5296bece9fd813811f3580c3cde
Marko Goronjic <goronjicmarko24@gmail.com>
2024-03-23 22:38:19 +0100

    - 01: Values and Variables

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,2 +1,16 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is fun!");
+
+let firstName = "Luffy";
+console.log(firstName);
+
+let luffy_zoro = "LZ";
+let $function = 29;
+
+let person = "Nami";
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "CyberSecDev";
+
+let job1 = "programmer";
+let job2 = "janitor";
