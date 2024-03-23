6e5eaaaa55533f39d954215c641fb3c2f09d15b7
Marko Goronjic <goronjicmarko24@gmail.com>
2024-03-23 22:57:19 +0100

    - 01: Data Types

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,8 +1,10 @@
+/*
 let js = "amazing";
 
 let firstName = "Luffy";
 console.log(firstName);
 
+// Variable name conventions
 let luffy_zoro = "LZ";
 let $function = 29;
 
@@ -14,3 +16,23 @@ let myCurrentJob = "CyberSecDev";
 
 let job1 = "programmer";
 let job2 = "janitor";
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Luffy');
+
+javascriptIsFun = "Yes!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = null;
+console.log(year);
+console.log(typeof null);
